a99e8b72e35c24098e5eccaaad62bbcc23f07a35
TTatchell <tim.tatchell24@gmail.com>
2021-07-03 17:24:15 +1000

    upload

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -18,30 +18,39 @@ def second_supply_for_fourth_of_july(holiday_hash)
   #   }
   # }
   # return the second element in the 4th of July array
+  holiday_hash[:summer][:fourth_of_july][1]
 end
 
 def add_supply_to_winter_holidays(holiday_hash, supply)
   # holiday_hash is identical to the one above
   # add the second argument, which is a supply, to BOTH the
   # Christmas AND the New Year's arrays
-
+  holiday_hash[:winter][:christmas] << supply
+  holiday_hash[:winter][:new_years] << supply
+  
 end
 
 
 def add_supply_to_memorial_day(holiday_hash, supply)
   # again, holiday_hash is the same as the ones above
   # add the second argument to the memorial day array
-
+  holiday_hash[:spring][:memorial_day] << supply
 end
 
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
   # code here
   # remember to return the updated hash
+  holiday_hash[season][holiday_name] = supply_array
 
 end
 
 def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winter season
+  ret_arr = []
+  holiday_hash[:winter].each do |holiday, item|
+    ret_arr << item
+  end
+  ret_arr.flatten
 
 end
 
@@ -53,16 +62,58 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
+  holiday_hash.each do |season, item|
+    puts "#{season.capitalize}:"
+
+    item.each do |holiday, items|
+      holiday_name = holiday.to_s.split('_').map(&:capitalize).join(' ')
+      items = items.to_s.tr!('[]"', '')
+      
+      puts "  #{holiday_name}: #{items}"
+      
+    end
+
+    
+  end
+
 
 end
 
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
+  ret_arr = []
+  holiday_hash.each do |season, itemA|
+    itemA.each do |holiday, itemB|
+
+      if itemB.to_s.include? "BBQ" 
+        puts holiday
+        ret_arr << holiday
+      end
+    end
+  end
+  ret_arr
 
 end
 
+holiday_hash = {
+    :winter => {
+      :christmas => ["Lights", "Wreath"],
+      :new_years => ["Party Hats"]
+    },
+    :summer => {
+      :fourth_of_july => ["Fireworks", "BBQ"]
+    },
+    :fall => {
+      :thanksgiving => ["Turkey"]
+    },
+    :spring => {
+      :memorial_day => ["BBQ"]
+    }
+  }
 
+
+all_holidays_with_bbq(holiday_hash)
 
 
 
